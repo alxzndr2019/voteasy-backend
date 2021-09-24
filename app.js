572f63017be31900a94c5e7ed10844dff3d52229
Ohioze Omiunu Alex <ohiozeomiunualex@Ohiozes-MacBook-Pro.local>
2021-09-24 16:31:19 +0100

    fix

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
     }
   });
 app.use(cors({
-    origin:[`https://voteasy.netlify.app`],
+    origin:[process.env.ORIGIN],
     credentials:true,
 }));
 app.use("/auth", require("./routes/userRouter"));
