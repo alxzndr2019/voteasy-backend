46d23e66748f3ae5e2ad1755b1cce4cee4014003
Ohioze Omiunu Alex <ohiozeomiunualex@Ohiozes-MacBook-Pro.local>
2021-09-24 16:38:07 +0100

    fix2

--- app.js
+++ app.js
@@ -19,20 +19,7 @@ app.get('/', (req,res)=> res.send('Voting app Api is up and running'));
 server.listen(port,()=>console.log(`Voting system server running on port ${port}`));
 app.use(express.json());
 app.use(cookieParser());
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://voteasy.netlify.app');
-    res.setHeader('Access-Control-Request-Method', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Expose-Headers', 'Content-Type');
-  
-    if(req.method === 'OPTIONS') {
-      res.writeHead(200);
-      return res.end();
-    } else {
-      return next();
-    }
-  });
+
 app.use(cors({
     origin:[process.env.ORIGIN],
     credentials:true,
