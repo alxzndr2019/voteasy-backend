cff0e7e8c64de30791d766eb2508053fbbcf9817
Ohioze Omiunu Alex <ohiozeomiunualex@Ohiozes-MacBook-Pro.local>
2021-10-05 19:26:05 +0100

    good

--- app.js
+++ app.js
@@ -29,6 +29,9 @@ app.use(function(req, res, next) {
       'Access-Control-Allow-Headers',
       'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization'
     );
+
+    res.setHeader('Access-Control-Allow-Credentials','true');
+
   
     //  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   
