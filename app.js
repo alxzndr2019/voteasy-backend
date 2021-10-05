2c2aca5ca55938c5ab19c17f564217e28d1eb0ef
Ohioze Omiunu Alex <ohiozeomiunualex@Ohiozes-MacBook-Pro.local>
2021-10-05 15:29:35 +0100

    allow

--- app.js
+++ app.js
@@ -21,14 +21,14 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(function(req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', process.env.ORIGIN);
+    res.setHeader('Access-Control-Allow-Origin','*');
   
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   
     // Request headers you wish to allow
     res.setHeader(
-      'Access-Control-Allow-Headers',
+      'Access-Control-Allow-Headers', 
       'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization'
     );
   
